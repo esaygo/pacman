eb8e6fd0f7b2e393604f2fc62abd300896e61ec8
esaygo <elena.saygo@gmail.com>
2016-04-01 10:59:48 -0700

    direction working, ghost in

--- js/jquery.js
+++ js/jquery.js
@@ -1,77 +1,150 @@
 $(document).ready(function() {
 
-  // to do:
-  // 1) display the world of bricks/coin
-  // 2) move pacman up and down
-  var world = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 2, 1, 1, 2, 1, 1, 2],
-    [2, 1, 1, 2, 1, 1, 2, 1, 1, 2],
-    [2, 1, 1, 2, 1, 1, 2, 1, 1, 2],
-    [2, 1, 1, 2, 2, 2, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-  ];
-  var score = 0;
-  var pacman = {
-    x: 1,
-    y: 1
-  }
+    // to do:
+    // 1) display the world of bricks/coin
+    // 2) move pacman up and down
+    var world = [
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      [2, 1, 3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 2, 0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 0, 2, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 0, 0, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 4, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 0, 0, 0, 0, 1, 2, 2, 2, 0, 1, 1, 1, 5, 1, 6, 1, 1, 0, 2, 2, 2, 1, 0, 0, 0, 0, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+      [2, 1, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 1, 2],
+      [2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2],
+      [2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 2],
+      [2, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 1, 2],
+      [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2],
+      [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+      [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ];
 
-  function displayWorld() {
-    var output = '';
-    for (var i = 0; i < world.length; i++) {
-      output += '\n<div class="row">\n';
-      for (var j = 0; j < world[i].length; j++) {
-        if (world[i][j] == 2)
-          output += "<div class='brick'></div>";
-        else if (world[i][j] == 1)
-          output += "<div class='coin'></div>";
-        if (world[i][j] == 0)
-          output += "<div class='empty'></div>";
+    var pacman = {
+      x: 1,
+      y: 1,
+      score: 0,
+
+    }
+
+    function displayWorld() {
+      var output = '';
+      for (var i = 0; i < world.length; i++) {
+        output += '\n<div class="row">\n';
+        for (var j = 0; j < world[i].length; j++) {
+          if (world[i][j] == 2) {
+            output += "<div class='brick'></div>";
+          }
+          // if (world[i][j] == 4) {
+          //   output += "<div class='brick boundary'></div>";
+          // }
+          if (world[i][j] == 1) {
+            output += "<div class='coin'></div>";
+          }
+          if (world[i][j] == 0) {
+            output += "<div class='empty'></div>";
+          }
+          if (world[i][j] == 3) {
+            // alert("cherry");
+            output += "<div class='cherry'></div>"
+          }
+          if (world[i][j] == 4) {
+            // alert("cherry");
+            output += "<div class='blue_ghost'></div>"
+          }
+        }
+        output += '\n</div>';
       }
-      output += '\n</div>';
-    }
-    // console.log(output);
-    document.getElementById('world').innerHTML = output;
-  }
-
-  function displayPacman() {
-    var pac = document.getElementById('pacman');
-    pac.style.top = pacman.y*20 + "px";
-    pac.style.left = pacman.x*20 + "px";
-  }
-  function displayScore() {
-    var pac = document.getElementById('score');
-    score.innerHTML = score;
-
-  }
-  displayWorld();
-  displayPacman();
-
-  document.onkeydown = function(e) {
-    if (e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2) {
-      pacman.x--;
-      console.log(pacman.x);
-    } else if (e.keyCode == 39 && world[pacman.y][pacman.x+1] !=2) {
-      pacman.x++;
-    } else if (e.keyCode == 38 && world[pacman.y+1][pacman.x]) {
-      pacman.y--;
-    } else if (e.keyCode == 40 && world[pacman.y-1][pacman.x]) {
-      pacman.y++;
+      // console.log(output);
+      document.getElementById('world').innerHTML = output;
     }
 
-    if(world[pacman.y][pacman.x] == 1) {
-      world[pacman.y][pacman.x] = 0;
-      score +=10;
-      displayWorld();
-      displayScore();
-      console.log(score);
+    function displayPacman() {
+      var pac = document.getElementById('pacman');
+      pac.style.top = pacman.y * 20 + "px";
+      pac.style.left = pacman.x * 20 + "px"
     }
+
+    displayWorld();
     displayPacman();
+
+    document.onkeydown = function(e) {
+        // console.log(e.which);
+        console.log(pacman.direction);
+        console.log($('#pacman').getRotateAngle());
+        if (e.keyCode == 37) { //left boundary
+          $('#pacman').rotate(180);
+
+        if (world[pacman.y][pacman.x - 1] != 2) {
+          pacman.x--;
+        }
+        pacman.direction = "left";
+        console.log(pacman.direction);
+
+      } else if (e.keyCode == 39) { //right boundary
+        console.log(pacman.direction);
+        console.log($('#pacman').getRotateAngle());
+
+      $('#pacman').rotate(0);
+    if (world[pacman.y][pacman.x + 1] != 2) {
+      pacman.x++;
+    }
+    pacman.direction = "right";
+    console.log(pacman.direction);
+
+  } else if (e.keyCode == 38) { //up boundary
+    console.log(pacman.direction);
+    console.log($('#pacman').getRotateAngle());
+
+  $('#pacman').rotate(270);
+  if (world[pacman.y - 1][pacman.x] != 2) {
+    pacman.y--;
   }
+  pacman.direction = "up"; console.log(pacman.direction);
+}
+else if (e.keyCode == 40) { //down boundary
+  console.log(pacman.direction);
+  console.log($('#pacman').getRotateAngle());
+
+$('#pacman').rotate(90);
+if (world[pacman.y + 1][pacman.x] != 2) {
+  pacman.y++;
+}
+pacman.direction = "down";
+console.log(pacman.direction);
+}
+
+if (world[pacman.y][pacman.x] == 1) {
+  world[pacman.y][pacman.x] = 0;
+  displayWorld();
+}
+if (world[pacman.y][pacman.x] == 3) {
+  // alert('foud cherry');
+  world[pacman.y][pacman.x] = 0;
+  pacman.score += 50;
+  document.getElementById('score').innerHTML = pacman.score;
+  displayWorld();
+}
+
+displayPacman();
+}
 
 
 
